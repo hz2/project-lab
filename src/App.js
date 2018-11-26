26b1ac47f9eb880fde05a4e7b7099d02d9454fd6
huc <ht@live.se>
2018-11-27 00:45:25 +0800

    fx

--- src/App.js
+++ src/App.js
@@ -4,28 +4,5 @@ import { Button } from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <Button type="primary">Button</Button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
+const App = () => <div>1</div>;
 export default App;
