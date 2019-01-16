1621d3451dcc64c48fd41f2b5cbe72bcd896ec48
huc <ht@live.se>
2019-01-17 00:14:13 +0800

    fix build

--- config-overrides.js
+++ config-overrides.js
@@ -1,22 +1,16 @@
 const {
-  injectBabelPlugin
-} = require('react-app-rewired');
-const rewireLess = require('react-app-rewire-less');
-module.exports = function override(config, env) {
-  config = injectBabelPlugin(
-    ['import', {
-      libraryName: 'antd',
-      libraryDirectory: 'es',
-      style: true
-    }], // change importing css to less
+  override,
+  fixBabelImports,
+  addLessLoader,
+} = require("customize-cra");
 
-    config
-  );
-  config = rewireLess.withLoaderOptions({
-    modifyVars: {
-      "@primary-color": "#00bbbb"
-    },
+
+module.exports = override(
+  fixBabelImports("import", {
+    libraryName: "antd", libraryDirectory: "es", style: true // change importing css to less
+  }),
+  addLessLoader({
     javascriptEnabled: true,
-  })(config, env);
-  return config;
-};
+    modifyVars: { "@primary-color": "#00bbbb" }
+  })
+);
