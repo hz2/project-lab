ca7164ab451d2e021be5d33630c9c92b470fbd64
huc <ht@live.se>
2019-01-24 00:59:39 +0800

    修复 connected-react-router-6

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux'
+import { connectRouter } from 'connected-react-router'
 import counter from './counter'
+import history from './history'
 
 export default combineReducers({
+  router: connectRouter(history),
   counter
 })
