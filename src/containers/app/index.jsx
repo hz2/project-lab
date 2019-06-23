671861bc70c3eaf8b33536ab6f8dbace37dc3eab
huc <ht@live.se>
2019-06-23 23:56:58 +0800

    更新符号

--- src/containers/app/index.jsx
+++ src/containers/app/index.jsx
@@ -2,23 +2,20 @@ import React from 'react'
 // import { Route, Link } from 'react-router-dom'
 import { Route, Switch } from 'react-router-dom'
 import Home from '../home'
-import About from '../about'
 import idc from '../idcard'
 import qs from '../qs'
+import svg from '../svg'
 
 const App = () => (
   <div>
-    <header>
-      {/* <Link to="/">Home</Link>
-      <Link to="/about-us">About</Link> */}
-    </header>
+    <header></header>
 
     <main>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/about-us" component={About} />
         <Route exact path="/idc" component={idc} />
         <Route exact path="/qs" component={qs} />
+        <Route exact path="/svg" component={svg} />
       </Switch>
     </main>
   </div>
---
+++ src/containers/home/index.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { push } from 'connected-react-router'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { Button } from 'antd'
+import {
+  increment,
+  incrementAsync,
+  decrement,
+  decrementAsync
+} from '../../modules/counter'
+
+const Home = props => (
+  <div>
+    <h1>Home</h1>
+    <p>Count: {props.count}</p>
+
+    <p>
+      <Button type="primary" onClick={props.increment}>
+        Increment
+      </Button>
+      <Button
+        type="primary"
+        onClick={props.incrementAsync}
+        disabled={props.isIncrementing}>
+        Increment Async
+      </Button>
+    </p>
+
+    <p>
+      <Button type="primary" onClick={props.decrement}>
+        Decrement
+      </Button>
+      <Button
+        type="primary"
+        onClick={props.decrementAsync}
+        disabled={props.isDecrementing}>
+        Decrement Async
+      </Button>
+    </p>
+
+    <p>
+      <Button type="primary" onClick={() => props.changePage()}>
+        Go to about page via redux
+      </Button>
+    </p>
+    <p>
+      <Button type="primary" onClick={() => props.history.push('/idc')}>
+        ID Card
+      </Button>{' '}
+      |{' '}
+      <Button type="primary" onClick={() => props.history.push('/qs')}>
+        QS
+      </Button>
+    </p>
+  </div>
+)
+
+const mapStateToProps = ({ counter }) => ({
+  count: counter.count,
+  isIncrementing: counter.isIncrementing,
+  isDecrementing: counter.isDecrementing
+})
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      increment,
+      incrementAsync,
+      decrement,
+      decrementAsync,
+      changePage: () => push('#/about')
+    },
+    dispatch
+  )
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home)
