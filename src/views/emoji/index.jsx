8b7e48e0c16656f0af026e20a3737da6d6796104
h2 <ht@live.se>
2020-10-20 23:42:03 +0800

    try to user queue

--- src/views/emoji/index.jsx
+++ src/views/emoji/index.jsx
@@ -1,17 +1,57 @@
 import React from 'react'
 import './style.less'
 
-let output = (len, start) => {
-  return new Array(len)
-    .fill(1)
-    .map((x, i) => String.fromCodePoint(start + i))
-    .join(' ')
+// let output = (len, start) => {
+//   return new Array(len)
+//     .fill(1)
+//     .map((x, i) => String.fromCodePoint(start + i))
+//     .join(' ')
+// }
+
+const output = Array.from(Array(5000), (x, i) => (125000 + i).toString(16))
+
+const openDown = (name, url, event, i) => {
+  event.preventDefault()
+  fetch(url, { mode: 'cors' })
+    .then(response => {
+      if (response.ok) {
+        console.log('index==>', i)
+        return response.blob()
+      }
+    })
+    .then(r => {
+      if (!r) return
+      let file = new FileReader()
+      file.onload = e => {
+        let el = document.createElement('a')
+        el.setAttribute('href', e.target.result)
+        el.setAttribute('download', name)
+        if (document.createEvent) {
+          var event = document.createEvent('MouseEvents')
+          event.initEvent('click', true, true)
+          el.dispatchEvent(event)
+        } else {
+          el.click()
+        }
+      }
+      file.readAsDataURL(r)
+    })
+    .catch(err => console.error(new Error(err)))
+}
+
+const downloadlist = event => {
+  output.forEach((x, i) => {
+    const name = x + '.svg'
+    const url = `https://abs-0.twimg.com/emoji/v2/svg/${x}.svg`
+    openDown(name, url, event, i)
+  })
 }
 
 const About = () => (
   <div className="container">
     <div className="symbol">
-      <h1>太玄经</h1>
+      <button onClick={event => downloadlist(event)}>下载</button>
+      {/* <h1>太玄经</h1>
       <h1>{output(87, 0x1d300)}</h1>
       <hr />
       <h1>爻</h1>
@@ -23,7 +63,7 @@ const About = () => (
       <h1>domino</h1>
       <h1>{output(100, 0x1f030)}</h1>
       <h1>麻将</h1>
-      <h1>{output(44, 0x1f000)}</h1>
+      <h1>{output(44, 0x1f000)}</h1> */}
     </div>
     {/* <p>Did you get here via Redux?</p> */}
   </div>
