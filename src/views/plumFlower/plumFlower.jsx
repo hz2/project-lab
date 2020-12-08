a0ba6669059f09981dcc6340f9f33855169f7abf
h2 <ht@live.se>
2020-12-09 00:14:29 +0800

    add text

--- src/views/plumFlower/plumFlower.jsx
+++ src/views/plumFlower/plumFlower.jsx
@@ -1,78 +1,137 @@
 import React from 'react'
 
-const width = 90
-const half = width / 2
+const w = 100
+const half = w / 2
 const subtense = Math.tan((22.5 * Math.PI) / 180) * half
 const p1 = half - subtense
 const p2 = half + subtense
 
-const list = [
+const obj = {
+  qian: {
+    trigrams: '☰',
+    text: '乾',
+    color: '#ff6c00'
+  },
+  dui: {
+    trigrams: '☱',
+    text: '兑',
+    color: '#d0d0d0'
+  },
+  li: {
+    trigrams: '☲',
+    text: '離',
+    color: '#d90505'
+  },
+  zhen: {
+    trigrams: '☳',
+    text: '震',
+    color: '#00f0a1'
+  },
+  xun: {
+    trigrams: '☴',
+    text: '巽',
+    color: '#00b076'
+  },
+  kan: {
+    trigrams: '☵',
+    text: '坎',
+    color: '#5b5b5b'
+  },
+  gen: {
+    trigrams: '☶',
+    text: '艮',
+    color: '#e3aa00'
+  },
+  kun: {
+    trigrams: '☷',
+    text: '坤',
+    color: '#fdcd3c'
+  }
+}
+
+const r = Math.sqrt(Math.pow((2 / 6) * w, 2) / 2)
+
+const listOrig = [
   {
     id: 'nn',
     d: ['M', half, half, p1, 0, p2, 0, 'z'],
-    color: 'red'
+    pos: [half, (1 / 6) * w]
   },
   {
     id: 'ne',
-    d: ['M', half, half, p2, 0, width, p1, 'z'],
-    color: 'orange'
+    d: ['M', half, half, p2, 0, w, p1, 'z'],
+    pos: [(1 / 2) * w + r, (1 / 2) * w - r]
   },
   {
     id: 'ee',
-    d: ['M', half, half, width, p1, width, p2, 'z'],
-    color: 'yellow'
+    d: ['M', half, half, w, p1, w, p2, 'z'],
+    pos: [(5 / 6) * w, (1 / 2) * w]
   },
   {
     id: 'se',
-    d: ['M', half, half, width, p2, p2, width, 'z'],
-    color: 'green'
+    d: ['M', half, half, w, p2, p2, w, 'z'],
+    pos: [(1 / 2) * w + r, (1 / 2) * w + r]
   },
 
   {
     id: 'ss',
-    d: ['M', half, half, p2, width, p1, width, 'z'],
-    color: 'cyan'
+    d: ['M', half, half, p2, w, p1, w, 'z'],
+    pos: [half, (5 / 6) * w]
   },
   {
     id: 'sw',
-    d: ['M', half, half, p1, width, 0, p2, 'z'],
-    color: 'blue'
+    d: ['M', half, half, p1, w, 0, p2, 'z'],
+    pos: [(1 / 2) * w - r, (1 / 2) * w + r]
   },
   {
     id: 'ww',
     d: ['M', half, half, 0, p2, 0, p1, 'z'],
-    color: 'purple'
+    pos: [(1 / 6) * w, (1 / 2) * w]
   },
   {
     id: 'nw',
     d: ['M', half, half, 0, p1, p1, 0, 'z'],
-    color: 'gray'
+    pos: [(1 / 2) * w - r, (1 / 2) * w - r]
   }
 ]
 
+const houtian = ['li', 'kun', 'dui', 'qian', 'kan', 'gen', 'zhen', 'xun']
+const list = listOrig.map((x, i) => Object.assign(x, obj[houtian[i]]))
 const bagua = (
   <svg
-    width="600"
-    height="600"
-    viewBox={[0, 0, width, width].join(' ')}
+    w="800"
+    height="800"
+    viewBox={[0, 0, w, w].join(' ')}
     xmlns="http://www.w3.org/2000/svg">
     {list.map(x => (
-      <path
-        id={x.id}
-        d={x.d.join(' ')}
-        fill={x.color}
-        stroke="#000"
-        stroke-width="0px"
-      />
+      <g>
+        <path
+          id={x.id}
+          d={x.d.join(' ')}
+          fill={x.color}
+          stroke="#000"
+          stroke-width="0px"
+        />
+        <text
+          color="#fff"
+          font-size="8px"
+          x={x.pos[0]}
+          y={x.pos[1]}
+          text-anchor="middle"
+          dominant-baseline="middle">
+          <tspan>{x.text}</tspan>
+          <tspan>{x.trigrams}</tspan>
+        </text>
+      </g>
     ))}
   </svg>
 )
 
-const About = () => (
+const Yi = () => (
   <div>
     <h1>Yi</h1>
     <div style={{ margin: '25px' }}>{bagua}</div>
   </div>
 )
 
-export default About
+export default Yi
