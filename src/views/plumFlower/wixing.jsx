47542f600bef0a9d361526ab6d683896410aa1f1
h2 <ht@live.se>
2021-01-01 02:37:09 +0800

    wuxing shengke

--- src/views/plumFlower/wixing.jsx
+++ src/views/plumFlower/wixing.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+const sinTheta = th => Math.sin((th * Math.PI) / 180)
+const cosTheta = th => Math.cos((th * Math.PI) / 180)
 const genPos = (half, r, type) => {
   const base = [
     {
@@ -22,78 +24,129 @@ const genPos = (half, r, type) => {
       color: '#fdcd3c'
     }
   ]
-  const sameObj = {
-    pos: [half, half - r * 2],
-    textTransform: `translate(0,${r / 6})`,
-    textPos: [half, half]
-  }
   let posArr = []
-  const sinTheta = th => Math.sin((th * Math.PI) / 180)
-  const cosTheta = th => Math.cos((th * Math.PI) / 180)
 
   if (type === 'wuxing') {
     posArr = [
       {
-        transform: `rotate(${2 * 72 - 18},${half},${half})`,
-        textTransform: `translate(${cosTheta(36) * r * 2},${sinTheta(36) *
-          r *
-          2 +
-          r / 6})`
+        pos: [half + cosTheta(36) * r * 2, half + sinTheta(36) * r * 2]
       },
       {
-        transform: `rotate(${3 * 72 - 18},${half},${half})`,
-        textTransform: `translate(${cosTheta(72) * r * 2 * -1},${sinTheta(72) *
-          r *
-          2 +
-          r / 6})`
+        pos: [half - cosTheta(72) * r * 2, half + sinTheta(72) * r * 2]
       },
       {
-        transform: `rotate(${4 * 72 - 18},${half},${half})`,
-        textTransform: `translate(${-r * 2},${r / 6})`
+        pos: [half - r * 2, half]
       },
       {
-        transform: `rotate(${0 - 18},${half},${half})`,
-        textTransform: `translate(${cosTheta(72) * r * 2 * -1},${sinTheta(72) *
-          r *
-          2 *
-          -1 +
-          r / 6})`
+        pos: [half - cosTheta(72) * r * 2, half - sinTheta(72) * r * 2]
       },
       {
-        transform: `rotate(${72 - 18},${half},${half})`,
-        textTransform: `translate(${cosTheta(36) * r * 2},${sinTheta(36) *
-          r *
-          2 *
-          -1 +
-          r / 6})`
+        pos: [half + cosTheta(36) * r * 2, half - sinTheta(36) * r * 2]
       }
     ]
   } else {
     posArr = [
       {
-        pos: [half + r * 2, half],
-        textPos: [half + r * 2, half]
+        pos: [half + r * 2, half]
       },
       {
-        pos: [half, half + r * 2],
-        textPos: [half, half + r * 2]
+        pos: [half, half + r * 2]
       },
       {
-        pos: [half - r * 2, half],
-        textPos: [half - r * 2, half]
+        pos: [half - r * 2, half]
       },
       {
-        pos: [half, half - r * 2],
-        textPos: [half, half - r * 2]
+        pos: [half, half - r * 2]
       },
       {
-        pos: [half, half],
-        textPos: [half, half]
+        pos: [half, half]
       }
     ]
   }
 
-  return posArr.map((x, i) => Object.assign({}, sameObj, base[i], x))
+  return posArr.map((x, i) => Object.assign({}, base[i], x))
+}
+
+const arrowSheng = (half, r, i) => (
+  <>
+    <path
+      d={[
+        'M',
+        half + cosTheta(36) * r * 2,
+        half - sinTheta(36) * r * 2 + r * 0.6,
+        half + cosTheta(36) * r * 2,
+        half + sinTheta(36) * r * 2 - r * 0.6,
+        half + cosTheta(36) * r * 2 - r / 6,
+        half + sinTheta(36) * r * 2 - r * 0.8,
+        half + cosTheta(36) * r * 2,
+        half + sinTheta(36) * r * 2 - r * 0.6,
+        half + cosTheta(36) * r * 2 + r / 6,
+        half + sinTheta(36) * r * 2 - r * 0.8
+      ].join(' ')}
+      transform={`rotate(${i * 72},${half},${half})`}
+      fill="none"
+      stroke="green"
+      strokeWidth={half / 90}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <text
+      color="green"
+      fontSize={half / 15}
+      fontWeight="bold"
+      x={half + cosTheta(36) * r * 2.5}
+      y={half}
+      textAnchor="middle"
+      dominantBaseline="middle"
+      transform={`rotate(${i * 72},${half},${half})`}>
+      <tspan>生</tspan>
+    </text>
+  </>
+)
+
+const arrowKe = (half, r, i) => {
+  const arrowKeTheta = (Math.atan(5 / 6) * 180) / Math.PI
+  const arrowKeLineC = Math.sqrt(Math.pow(r / 6, 2) + Math.pow(r / 5, 2))
+  return (
+    <>
+      <path
+        d={[
+          'M',
+          half - r * 2 + cosTheta(18) * 0.6 * r,
+          half - sinTheta(18) * 0.6 * r,
+          half + cosTheta(36) * r * 2 - cosTheta(18) * 0.6 * r,
+          half - sinTheta(36) * r * 2 + sinTheta(18) * 0.6 * r,
+          // 箭头点
+          half +
+            cosTheta(36) * r * 2 -
+            cosTheta(18) * 0.6 * r -
+            sinTheta(72 - arrowKeTheta) * arrowKeLineC,
+          half -
+            sinTheta(36) * r * 2 +
+            sinTheta(18) * 0.6 * r +
+            cosTheta(72 - arrowKeTheta) * arrowKeLineC,
+          // 端点
+          half + cosTheta(36) * r * 2 - cosTheta(18) * 0.6 * r,
+          half - sinTheta(36) * r * 2 + sinTheta(18) * 0.6 * r,
+          // 箭头点
+          half +
+            cosTheta(36) * r * 2 -
+            cosTheta(18) * 0.6 * r -
+            sinTheta(108 - arrowKeTheta) * arrowKeLineC,
+          half -
+            sinTheta(36) * r * 2 +
+            sinTheta(18) * 0.6 * r -
+            cosTheta(108 - arrowKeTheta) * arrowKeLineC
+        ].join(' ')}
+        transform={`rotate(${i * 72},${half},${half})`}
+        fill="none"
+        stroke="red"
+        strokeWidth={half / 90}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </>
+  )
 }
 
 const Wuxing = props => {
@@ -116,11 +169,29 @@ const Wuxing = props => {
           <text
             color="#fff"
             fontSize={w / 25}
-            transform={x.textTransform}
-            x={x.textPos[0]}
-            y={x.textPos[1]}
+            transform={`translate(0,${r / 6})`}
+            x={x.pos[0]}
+            y={x.pos[1]}
             textAnchor="middle">
             <tspan>{x.num}</tspan>
+          </text>
+        </g>
+      ))}
+      {[0, 1, 2, 3, 4].map(x => (
+        <g>
+          {arrowSheng(half, r, x)}
+          {arrowKe(half, r, x)}
+
+          <text
+            color="red"
+            fontSize={half / 12}
+            fontWeight="bold"
+            x={half}
+            y={half}
+            textAnchor="middle"
+            dominantBaseline="middle"
+            transform={`translate(0,${r / 6})`}>
+            <tspan>克</tspan>
           </text>
         </g>
       ))}
