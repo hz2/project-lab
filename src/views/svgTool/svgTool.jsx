97753a78bac79fbe1f0d84c83b39b81db267716e
h2 <ht@live.se>
2021-01-19 23:56:56 +0800

    prototype

--- src/views/svgTool/svgTool.jsx
+++ src/views/svgTool/svgTool.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { Tabs, Button, Upload } from 'antd'
-import { getFileInfo } from 'prettier'
+import { Tabs, Upload, Button, message } from 'antd'
+import { UploadOutlined } from '@ant-design/icons'
 const { TabPane } = Tabs
+const parseString = require('xml2js').parseString
+const xml2js = require('xml2js')
+import './svgTool.less'
 
 const SvgTool = () => {
   const [a, setA] = useState('')
@@ -9,11 +12,36 @@ const SvgTool = () => {
     setA(1)
     console.log('a', a)
   }, [])
+  const props = {
+    name: 'file',
+    beforeUpload: file => {
+      console.log('file', file)
+      if (file) {
+        var reader = new FileReader()
+        reader.readAsText(file, 'UTF-8')
+        reader.onload = evt => {
+          const xmlStr = evt.target.result
+          parseString(xmlStr, { trim: true }, function(err, result) {
+            const arr = result.svg.symbol
 
-  const getFileInfo = e => {
-    console.log('e', e)
+            const svgList = arr
+              .map(x => {
+                var builder = new xml2js.Builder()
+                var xml = builder.buildObject({ svg: x })
+                return xml
+              })
+              .join('')
+
+            document.querySelector('#domResult').innerHTML = svgList
+          })
+        }
+        reader.onerror = e => {
+          console.error('file err', e)
+        }
+      }
+      return false
+    }
   }
-
   return (
     <div className="svgTool common-box">
       <Tabs
@@ -23,10 +51,11 @@ const SvgTool = () => {
         }}>
         <TabPane tab="Symbol" key="1">
           <div className="buttons">
-            <Upload type="primary" onChange={e => getFileInfo(e)}>
-              选择文件
+            <Upload {...props}>
+              <Button icon={<UploadOutlined />}>Click to Upload</Button>
             </Upload>
           </div>
+          <div id="domResult"></div>
         </TabPane>
       </Tabs>
     </div>
