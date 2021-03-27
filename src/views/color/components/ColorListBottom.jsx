efb4cc99347a88c619cc4d1056bd2442266277b5
h2 <ht@live.se>
2021-03-27 22:35:16 +0800

    try to more readable

--- src/views/color/components/ColorListBottom.jsx
+++ src/views/color/components/ColorListBottom.jsx
@@ -4,25 +4,18 @@ import { Radio, message } from 'antd'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { hsl2rgb } from './colors'
 
-const genShowList = (val, color, setShowCount, setShowListDom) => {
-  const { h, s, l } = color
+const genArr2 = len => Array.from(Array(len + 1), (x, i) => i * 1)
+const hsl2hex = arr =>
+  '#' +
+  hsl2rgb(arr)
+    .slice(0, 3)
+    .map(x => x.toString(16).padStart(2, 0))
+    .join('')
+const genHexList = (fn, val) => genArr2(val).map(x => hsl2hex(fn(x)))
 
-  const onChangeFn = ({ target: { value } }) =>
-    genShowList(value * 1, color, setShowCount, setShowListDom)
-  setShowCount(val)
-  const genArr2 = len => Array.from(Array(len + 1), (x, i) => i * 1)
-
-  const hsl2hex = arr =>
-    '#' +
-    hsl2rgb(arr)
-      .slice(0, 3)
-      .map(x => x.toString(16).padStart(2, 0))
-      .join('')
-
-  const genHexList = fn => genArr2(val).map(x => hsl2hex(fn(x)))
-
-  const cpList2 = fn =>
-    genHexList(fn).map((x, i) => (
+const CopyList2 = ({ fn, val }) => (
+  <div className="cat">
+    {genHexList(fn, val).map((x, i) => (
       <CopyToClipboard
         text={x}
         title={x}
@@ -34,48 +27,62 @@ const genShowList = (val, color, setShowCount, setShowListDom) => {
             backgroundColor: x
           }}></div>
       </CopyToClipboard>
-    ))
+    ))}
+  </div>
+)
 
-  const showH2 = cpList2(x => [(360 / val) * x, s, l])
-  const showS2 = cpList2(x => [h, ((100 / val) * x) / 100, l])
-  const showL2 = cpList2(x => [h, s, ((100 / val) * x) / 100])
-
-  setShowListDom(
-    <div className="showList">
-      <div className="action">
-        <Radio.Group
-          defaultValue="10"
-          buttonStyle="solid"
-          onChange={onChangeFn}>
-          <Radio.Button value="10">10</Radio.Button>
-          <Radio.Button value="15">15</Radio.Button>
-          <Radio.Button value="20">20</Radio.Button>
-          <Radio.Button value="30">30</Radio.Button>
-        </Radio.Group>
-      </div>
-      <div className={'cats count' + val}>
-        <div className="cat">{showH2}</div>
-        <div className="cat">{showS2}</div>
-        <div className="cat">{showL2}</div>
-      </div>
-    </div>
-  )
-}
-
-const ColorListBottom = props => {
-  const color = props.val
-
-  const [showCount, setShowCount] = useState(10)
-
-  const [showListDom, setShowListDom] = useState(null)
+const ColorList = ({ val, color }) => {
+  const [colorState, setColor] = useState(color)
+  const [valState, setVal] = useState(val)
 
   useEffect(() => {
-    genShowList(showCount, color, setShowCount, setShowListDom)
-  }, [showCount, color, setShowCount, setShowListDom])
+    setColor(color)
+    setVal(val)
+  }, [color, val])
 
   return (
     <>
-      <div className="colorListBottom">{showListDom}</div>
+      <div className={'cats count' + valState}>
+        <CopyList2
+          fn={x => [(360 / valState) * x, colorState.s, colorState.l]}
+          val={valState}
+        />
+        <CopyList2
+          fn={x => [colorState.h, ((100 / val) * x) / 100, colorState.l]}
+          val={valState}
+        />
+        <CopyList2
+          fn={x => [colorState.h, colorState.s, ((100 / val) * x) / 100]}
+          val={valState}
+        />
+      </div>
+    </>
+  )
+}
+
+const Actiongroup = ({ onChangeFn }) => (
+  <div className="showList">
+    <div className="action">
+      <Radio.Group defaultValue="10" buttonStyle="solid" onChange={onChangeFn}>
+        <Radio.Button value="10">10</Radio.Button>
+        <Radio.Button value="15">15</Radio.Button>
+        <Radio.Button value="20">20</Radio.Button>
+        <Radio.Button value="30">30</Radio.Button>
+      </Radio.Group>
+    </div>
+  </div>
+)
+
+const ColorListBottom = props => {
+  const color = props.val
+  const [showCount, setShowCount] = useState(10)
+  const onChangeFn = ({ target: { value } }) => setShowCount(value)
+  return (
+    <>
+      <div className="colorListBottom">
+        <Actiongroup onChangeFn={onChangeFn} />
+        <ColorList color={color} val={showCount} />
+      </div>
     </>
   )
 }
