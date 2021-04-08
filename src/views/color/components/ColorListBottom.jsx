42df1eea3653711692dbb711c1d61dc76fbee2fc
h2 <ht@live.se>
2021-04-08 23:31:46 +0800

    optimizing code

--- src/views/color/components/ColorListBottom.jsx
+++ src/views/color/components/ColorListBottom.jsx
@@ -13,12 +13,13 @@ const hsl2hex = arr =>
     .join('')
 const genHexList = (fn, count) => genArr2(count * 1).map(x => hsl2hex(fn(x)))
 
-const CopyList2 = ({ hexList, count }) => {
+const ColorArr = ({ fn, count }) => {
   const [dom, genDom] = useState(null)
   useEffect(() => {
+    const arr = genHexList(fn, count)
     genDom(
       <div className="cat">
-        {hexList.map((x, i) => (
+        {arr.map((x, i) => (
           <CopyToClipboard
             text={x}
             title={x}
@@ -33,7 +34,7 @@ const CopyList2 = ({ hexList, count }) => {
         ))}
       </div>
     )
-  }, [hexList, count])
+  }, [fn, count])
   return <>{dom}</>
 }
 
@@ -47,31 +48,15 @@ const ColorList = ({ count, color }) => {
   }, [color, count])
 
   return (
-    <>
-      <div className={'cats count' + countState}>
-        <CopyList2
-          hexList={genHexList(
-            x => [(360 / countState) * x, colorState.s, colorState.l],
-            countState
-          )}
-          count={countState}
-        />
-        <CopyList2
-          hexList={genHexList(
-            x => [colorState.h, ((100 / countState) * x) / 100, colorState.l],
-            countState
-          )}
-          count={countState}
-        />
-        <CopyList2
-          hexList={genHexList(
-            x => [colorState.h, colorState.s, ((100 / countState) * x) / 100],
-            countState
-          )}
-          count={countState}
-        />
-      </div>
-    </>
+    <div className={'cats count' + countState}>
+      {[
+        x => [(360 / countState) * x, colorState.s, colorState.l],
+        x => [colorState.h, ((100 / countState) * x) / 100, colorState.l],
+        x => [colorState.h, colorState.s, ((100 / countState) * x) / 100]
+      ].map((fn, i) => (
+        <ColorArr key={i} fn={fn} count={countState} />
+      ))}
+    </div>
   )
 }
 
