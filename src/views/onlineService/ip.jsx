8c288dc1d519a6f52d47e5b19a6d7f659bde4f4b
h2 <ht@live.se>
2021-04-30 23:58:11 +0800

    update

--- src/views/onlineService/ip.jsx
+++ src/views/onlineService/ip.jsx
@@ -1,7 +1,24 @@
 // https://reqbin.com/lib/ipInfo-api/yel1uw4m/ip-geolocation-api-example
 // https://devpal.co/
-import React from 'react'
-const Page = () => <div>TODO</div>
+import React, { useEffect, useState } from 'react'
+const Page = () => {
+  const [text, setText] = useState('')
+
+  useEffect(() => {
+    const url = 'https://respok.com/ipinfo_io/default'
+    fetch(url, { mode: 'cors' })
+      .then(response => response.json())
+      .then(r => {
+        console.log('r', r)
+        if (r) {
+          const { country, city, ip } = r
+          setText(`${country} ${city} ==> ${ip}`)
+        }
+      })
+      .catch(err => console.error(new Error(err)))
+  }, [])
+  return <div>{text}</div>
+}
 
 export default Page
 
