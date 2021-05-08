c34f16f4cebbf4a67b385ebff32c40dcc76ee33f
h2 <ht@live.se>
2021-05-08 23:52:36 +0800

    make a commit

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -13,7 +13,7 @@ const generatorList = [
   'lottery',
   'plumFlower'
 ]
-const cnverterList = ['encode', 'hex', 'queryString', 'svg']
+const cnverterList = ['encode', 'hex', 'currencyExchange', 'queryString', 'svg']
 const analyzerList = ['ipAddr']
 
 const genTitle = str =>
