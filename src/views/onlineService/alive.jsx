8d4a32b032f53af05e17b6ebd808c14ea6802ab9
h2 <ht@live.se>
2021-06-02 22:46:02 +0800

    fix err

--- src/views/onlineService/alive.jsx
+++ src/views/onlineService/alive.jsx
@@ -3,6 +3,8 @@ import { Input } from 'antd'
 
 const Page = () => {
   // is still alive
+  const url = ''
+  const urlChange = ()=>{}
   return (
     <div className="common-box">
       <Input value={url} onChange={urlChange} />
