a80841e3052a97f93fa8329ddacd7807275d7338
h2 <ht@live.se>
2021-06-15 00:14:51 +0800

    update

--- src/views/encode/dataURL.jsx
+++ src/views/encode/dataURL.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from 'react'
-import { Upload, Input, Button } from 'antd'
+import { Upload, Input, Button, Radio } from 'antd'
 import { UploadOutlined } from '@ant-design/icons'
 import './style.less'
 const { TextArea } = Input
 const DataUrl = () => {
   const [b64Str, setB64Str] = useState('')
+  const [radio, setRadio] = useState('b64')
   const props = {
     name: 'file',
     maxCount: 1,
     beforeUpload: file => false,
     onChange({ file, fileList }) {
       if (file.status !== 'uploading') {
-        const { originFileObj } = fileList[0] || {}
+        const file = fileList[0]
+        if (!file) {
+          return
+        }
+        const { originFileObj } = file
         const reader = new FileReader()
-        reader.readAsDataURL(originFileObj)
+        if (radio === 'text') {
+          reader.readAsText(originFileObj, 'UTF-8')
+        } else {
+          reader.readAsDataURL(originFileObj, 'UTF-8')
+        }
         reader.onload = () => {
           const result = reader.result
           setB64Str(result)
@@ -22,19 +31,23 @@ const DataUrl = () => {
     }
   }
 
+  const setType = ({ target: { value: val } }) => {
+    setRadio(val)
+  }
+
   return (
     <div className="p20">
       <div className="dataurl">
         <h2>Data URLs 转换</h2>
-
+        <Radio.Group defaultValue="b64" buttonStyle="solid" onChange={setType}>
+          <Radio.Button value="text">Text</Radio.Button>
+          <Radio.Button value="b64">Base64</Radio.Button>
+        </Radio.Group>
+        <h4>请选择文件</h4>
         <Upload {...props}>
           <Button icon={<UploadOutlined />}>Click to Upload</Button>
         </Upload>
-        <TextArea
-          placeholder={'{"id":"1","name":"querystring"}'}
-          rows={15}
-          value={b64Str}
-        />
+        <TextArea placeholder="请选择文件" rows={15} value={b64Str} />
       </div>
     </div>
   )
