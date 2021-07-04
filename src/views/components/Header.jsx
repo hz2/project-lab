166007981e3eb8de47e2835d1b3bd3233b77aa9f
h2 <ht@live.se>
2021-07-05 00:08:44 +0800

    add home icon

--- src/views/components/Header.jsx
+++ src/views/components/Header.jsx
@@ -1,10 +1,14 @@
 import { HomeOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
 import './header.less'
 
 const HeaderBar = () => {
   return (
     <div className="headerIcon">
-      <HomeOutlined />
+      <Link to="/">
+        {' '}
+        <HomeOutlined />
+      </Link>
     </div>
   )
 }
