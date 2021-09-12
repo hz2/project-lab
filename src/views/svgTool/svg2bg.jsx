3e64d96ba324641dfca0af7503df0722954f2c2a
h2 <ht@live.se>
2021-09-12 23:58:14 +0800

    update

--- src/views/svgTool/svg2bg.jsx
+++ src/views/svgTool/svg2bg.jsx
@@ -6,6 +6,7 @@ import {
   DeleteTwoTone
 } from '@ant-design/icons'
 import './svgTool.less'
+const { TextArea } = Input
 const Page = () => {
   const [url, setUrl] = useState('')
   const urlChange = ({ target: { value } }) => {
@@ -30,6 +31,9 @@ const Page = () => {
     //       ]
     //     })
     //   })
+    setInputObj({
+      text:`<svg opacity="1.0" fill="none" width="32" height="32" stroke-linecap="round" stroke-linejoin="round" stroke="#777" stroke-width="2" viewBox="0 0 32 32"><path d="M14 2 L14 6 M14 18 L14 30 M2 6 L2 18 24 18 30 12 24 6Z"></path></svg>`
+    })
   }
 
 
@@ -41,6 +45,18 @@ const Page = () => {
     beforeUpload: () => false,
     onPreview: () => false,
   }
+
+  const [inputObj, setInputObj] = useState({
+    text: '',
+    dataUrl: ""
+
+  })
+
+  const origTextInput = () => { 
+    setInputObj()
+  }
+  const dataUrlInput = () => { }
+
 
   return (
     <>
@@ -56,7 +72,23 @@ const Page = () => {
         </Button>
       </div>
       <div className="common-box">
-        <Input value={url} onChange={urlChange} />
+        <div className="text">输入 SVG 代码</div>
+        <TextArea
+          rows={6}
+          className="pct33"
+          placeholder='<?xml version=\"1.0\" encoding=\"UTF-8\"?>'
+          value={inputObj.text}
+          onChange={origTextInput}
+        />
+
+        <div className="text">转换结果</div>
+        <TextArea
+          rows={6}
+          className="pct33"
+          placeholder="data:image/svg+xml"
+          value={inputObj.dataUrl}
+          onChange={dataUrlInput}
+        />
       </div>
     </>
   )
