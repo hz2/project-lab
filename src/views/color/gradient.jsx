c2c59994b192aad0fa57936bf2100b15caba0344
h2 <ht@live.se>
2021-10-27 23:19:32 +0800

    add page

---
+++ src/views/color/gradient.jsx
@@ -0,0 +1,14 @@
+// gradient
+import React, { useState, useEffect } from 'react'
+import './color.less'
+import { Input, Button, Slider } from 'antd'
+import ColorListBottom from './components/ColorListBottom'
+import { colorStr2arr, hsl2rgb, rgb2hsl } from './components/colors'
+
+import { copyText } from "@libs/common"
+
+const GradientPage = () => {
+    return <>GradientPage</>
+}
+    
+export default GradientPage
