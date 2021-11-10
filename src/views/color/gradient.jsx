b0a2ff9d69a257d14666f39834ce8024d6222f21
h2@server <ht@live.se>
2021-11-10 15:56:45 +0000

    update

--- src/views/color/gradient.jsx
+++ src/views/color/gradient.jsx
@@ -3,9 +3,9 @@ import React, { useState, useEffect } from 'react'
 import './color.less'
 import { Input, Button, Slider } from 'antd'
 import ColorListBottom from './components/ColorListBottom'
-import { colorStr2arr, hsl2rgb, rgb2hsl } from './components/colors'
+// import { colorStr2arr, hsl2rgb, rgb2hsl } from './components/colors'
 
-import { copyText } from "@libs/common.js"
+// import { copyText } from "@libs/common.js"
 
 import ColorPicker from './components/colorPicker'
 
