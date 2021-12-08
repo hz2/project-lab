dc653077ee4904f320bdfc6ae590cd2f560430e9
h2 <ht@live.se>
2021-12-08 23:05:59 +0800

    update

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import { connectRouter, routerMiddleware } from 'connected-react-router'
+import { connectRouter, routerMiddleware, RouterAction} from 'connected-react-router'
 import thunk from 'redux-thunk'
 import rootReducer from './modules'
 import history from './modules/history'
@@ -7,6 +7,12 @@ import history from './modules/history'
 const initialState = {}
 const enhancers = []
 const middleware = [thunk, routerMiddleware(history)]
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__?: typeof compose;
+  }
+}
 
 if (process.env.NODE_ENV === 'development') {
   const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__
@@ -17,6 +23,10 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers)
+
+type Action = RouterAction 
+
+
 
 export default createStore(
   connectRouter(history)(rootReducer),
