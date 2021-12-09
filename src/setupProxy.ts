2d92e5ef69aa7d48b952953a46734b8e8b5f4cee
h2 <ht@live.se>
2021-12-09 23:13:33 +0800

    update

--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware')
 
-module.exports = function(app) {
+module.exports = function(app: any) {
   // app.use(proxy('/api', { target: 'http://localhost:5000/' }));
 }
