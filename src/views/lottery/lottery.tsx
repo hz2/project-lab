9175d6729eeee394cd490fd2888930f7c3940cd8
h2 <ht@live.se>
2021-12-20 00:30:32 +0800

    fix err

--- src/views/lottery/lottery.tsx
+++ src/views/lottery/lottery.tsx
@@ -12,30 +12,30 @@ interface IGenBall {
   ball: string
   arr?: number[]
 }
-const genBall = (len: number = 33): Promise<IGenBall> =>
-  new Promise((resolve, reject) => {
-    const ballArr = Array.from(Array(len), (x, i) =>
-      (i + 1).toString().padStart(2, '0')
-    )
-    // 天时 地利 人和
-    const tianshi = Date.now().toString(32)
-    const dili = [alpha, beta, gamma].map(x =>
-      Number(x)
-        .toString(32)
-        .substring(4)
-    )
-    const renhe = Math.random()
+const genBall = (len: number = 33): Promise<IGenBall> => new Promise((resolve, reject) => {
+  const ballArr = Array.from(Array(len), (x, i) =>
+    (i + 1).toString().padStart(2, '0')
+  )
+  // 天时 地利 人和
+  const tianshi = (Date.now() + Math.random() * 10e13).toString(32)
+  const dili = [alpha, beta, gamma].map(x =>
+    Number(x)
       .toString(32)
-      .substring(2)
-    const arr = [renhe, dili, tianshi]
-      .flat()
-      .map(x => parseInt((x || 0).toString(), 32))
-    // fn && fn(arr)
-    const intN = BigInt(arr.join(''))
-    const key = Number(intN % BigInt(len))
-    // console.log(intN, 'intN', ballArr, intN % BigInt(len));
-    resolve({ ball: ballArr[key], arr: arr })
-  })
+      .substring(4)
+  )
+  const renhe = Math.random()
+    .toString(32)
+    .substring(2)
+  const arr = [renhe, dili, tianshi]
+    .flat()
+    .map(x => parseInt((x || 0).toString(), 32))
+  const intN = BigInt(arr.join(''))
+  const key = Number(intN % BigInt(len))
+  // console.log(intN, 'intN', ballArr,key,  intN % BigInt(len));
+  resolve({ ball: ballArr[key], arr: arr })
+})
+
+const pickBall = (arr: string[], num: number) => Array.from(new Set(arr), x => Number(x)).slice(-1 * num).sort((a, b) => a - b)
 
 const handleOrientation = (event: DeviceOrientationEvent) => {
   alpha = event.alpha || ''
@@ -54,9 +54,9 @@ const LotteryPage = () => {
     twoColorBall()
   }, [])
 
-  const twoColorBall = (times?: number) => {
-    const genGroup = (key = 0) => {
-      const fullArr = new Set(
+  const twoColorBall = async (times?: number) => {
+    const genGroup = async (key = 0) => {
+      const fullArrPromise = new Set(
         Array.from(Array(30), async () => {
           const { arr, ball } = await genBall(33)
           if (arr) {
@@ -65,35 +65,31 @@ const LotteryPage = () => {
           return ball
         })
       )
-      const ball6 = [...Array.from(fullArr)].slice(-6).sort()
-      const ball1 = genBall(16)
+      const fullArr = await Promise.all(Array.from(fullArrPromise))
+      const ball6 = pickBall(fullArr, 6)
+      const ball1 = Number((await genBall(16)).ball)
       return (
         <div className="ballgroup" key={key}>
           {ball6.map((x, i) => (
-            <div className="ball redball" key={i}>
-              {Number(x)}
-            </div>
+            <div className="ball redball" key={i}>{x}</div>
           ))}
-          <div className="ball blueball">
-            {Number(ball1)}
-          </div>
+          <div className="ball blueball">{ball1}</div>
         </div>
       )
     }
     if (times) {
-      const list = (
-        <div className="groups">
-          {Array.from(Array(times), (x, i) => genGroup(i))}
-        </div>
-      )
+      const list = <div className="groups">
+        {await Promise.all(Array.from(Array(times), (x, i) => genGroup(i)))}
+      </div>
       palceBall(list)
     } else {
-      palceBall(genGroup())
+      const group = await genGroup()
+      palceBall(group)
     }
   }
-  const superLottery = (times?: number) => {
-    const genGroup = (key = 0) => {
-      const fullArr = new Set(
+  const superLottery = async (times?: number) => {
+    const genGroup = async (key = 0) => {
+      const fullArrPromise = new Set(
         Array.from(Array(30), async () => {
           const { arr, ball } = await genBall(35)
           if (arr) {
@@ -102,8 +98,9 @@ const LotteryPage = () => {
           return ball
         })
       )
-      const ball5 = [...Array.from(fullArr)].slice(-5).sort()
-      const fullArr2 = new Set(
+      const fullArr = await Promise.all(Array.from(fullArrPromise))
+      const ball5 = pickBall(fullArr, 5)
+      const fullArr2Promise = new Set(
         Array.from(Array(12), async () => {
           const { arr, ball } = await genBall(12)
           if (arr) {
@@ -112,18 +109,15 @@ const LotteryPage = () => {
           return ball
         })
       )
-      const ball2 = [...Array.from(fullArr2)].slice(-2).sort()
+      const fullArr2 = await Promise.all(Array.from(fullArr2Promise))
+      const ball2 = pickBall(fullArr2, 2)
       return (
         <div className="ballgroup" key={key}>
           {ball5.map((x, i) => (
-            <div className="ball blue2ball" key={i}>
-              {Number(x)}
-            </div>
+            <div className="ball blue2ball" key={i}>{x}</div>
           ))}
           {ball2.map((x, i) => (
-            <div className="ball yellowball" key={i}>
-              {Number(x)}
-            </div>
+            <div className="ball yellowball" key={i}>{x}</div>
           ))}
         </div>
       )
@@ -131,12 +125,13 @@ const LotteryPage = () => {
     if (times) {
       const list = (
         <div className="groups">
-          {Array.from(Array(times), (x, i) => genGroup(i))}
+          {await Promise.all(Array.from(Array(times), (x, i) => genGroup(i)))}
         </div>
       )
       palceBall2(list)
     } else {
-      palceBall2(genGroup())
+      const group = await genGroup()
+      palceBall2(group)
     }
   }
 
