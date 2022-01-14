8b32ae31728aef56c1f97b10fa6e434014663b23
h2 <ht@live.se>
2022-01-14 22:57:54 +0800

    update

--- src/views/onlineService/web.tsx
+++ src/views/onlineService/web.tsx
@@ -1,21 +1,49 @@
 import React, { useState } from 'react'
 import { Input, Button } from 'antd'
+import "./web.less";
 
 const Page = () => {
   const [url, setUrl] = useState('')
+  const [twObj, setTwObj] = useState({
+    link: ''
+  })
   const urlChange = ({
     target: { value }
   }: React.ChangeEvent<HTMLInputElement>) => {
-    console.log('val', value)
     setUrl(value)
+  }
+  const handleUrl = () => {
+    console.log('url',url );
+    
+    if (!url) {
+      return
+    }
+    // twitter
+    const tw = url.match(/pbs\.twimg\.com\/media\/(FJBhq7JakAAXztR)\?format=([a-z]+)&name=/i)
+    if (tw) {
+      const link = `https://box.p0t.top/media/${tw[1]}?format=${tw[2]}&name=orig`
+      setTwObj({
+        link
+      })
+
+    }
+
   }
   return (
     <div className="common-box">
-      <Input value={url} className='w400' onChange={urlChange} />
-      <Button type='primary' className='ml15'>提交</Button>
-      <Button type='primary' className='ml15'>二维码</Button>
+      <nav>
+        <Input value={url} className='w400' placeholder='https://www.' onChange={urlChange} />
+        <Button type='primary' className='ml15' onClick={() => handleUrl()}>提交</Button>
+        <Button type='primary' className='ml15'>二维码</Button>
+      </nav>
+      <section className='twipic'>
+        <img src={twObj?.link} alt="" />
+
+      </section>
+
     </div>
   )
 }
 
 export default Page
+// https://pbs.twimg.com/media/FJBhq7JakAAXztR?format=jpg&name=small
