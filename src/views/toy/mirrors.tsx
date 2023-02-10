fada35c2b7fbb868136e4d16d2257b3bc3799bea
h2 <ht@live.se>
2023-02-10 22:50:41 +0800

    update

--- src/views/toy/mirrors.tsx
+++ src/views/toy/mirrors.tsx
@@ -1,114 +1,114 @@
 import React, { useState } from 'react'
-import { Input, Select, Radio, message, RadioChangeEvent } from 'antd'
+import { Input } from 'antd'
 import './toy.less'
 const { TextArea } = Input
 
-interface IPathItem {
-    path: string
-    remark: string
-}
+// interface IPathItem {
+//     path: string
+//     remark: string
+// }
 
-interface ITypeItem {
-    zh?: string
-    name?: string
-    user?: string
-    passwdfile?: string
-    reg?: string
-    path: IPathItem[]
-}
+// interface ITypeItem {
+//     zh?: string
+//     name?: string
+//     user?: string
+//     passwdfile?: string
+//     reg?: string
+//     path: IPathItem[]
+// }
 
-const typeList = [
-    {
-        zh: '阿里云',
-        name: 'aliyun',
-        user: 'a@h2',
-        passwdfile: '~/.ssh/docker-alibabacloud',
-        reg: 'registry.cn-hangzhou.aliyuncs.com',
-        path: [
-            {
-                path: 'h2',
-                remark: '私有'
-            },
-            {
-                path: 'h7',
-                remark: '私有'
-            },
-            {
-                path: 'h9',
-                remark: '私有'
-            }
-        ]
-    },
-    {
-        zh: '腾讯云',
-        name: 'qcloud',
-        user: '100004236481',
-        passwdfile: '~/.ssh/docker-tencentcloud',
-        reg: 'ccr.ccs.tencentyun.com',
-        path: [
-            {
-                path: 'hzsq',
-                remark: '私有'
-            }
-        ]
-    },
-    {
-        zh: 'Docker hub',
-        name: 'dockerhub',
-        user: 'hzsq',
-        passwdfile: '~/.ssh/dockerioToken',
-        reg: '',
-        path: [
-            {
-                path: 'hzsq',
-                remark: '公有'
-            }
-        ]
-    },
-    {
-        zh: '华为云',
-        name: 'huawei',
-        user: 'cn-north-4@XRV15LDEFWCFLDH7GDXY',
-        passwdfile: '~/.ssh/docker-huaweicloud',
-        reg: 'swr.cn-north-4.myhuaweicloud.com',
-        path: [
-            {
-                path: 'hz',
-                remark: '私有'
-            }
-        ]
-    },
-    {
-        zh: '百度云',
-        name: 'baidu',
-        user: '18999d5116464463a9cbc2c7f8f3cf01',
-        passwdfile: '~/.ssh/docker-baiducloud',
-        reg: 'registry.baidubce.com',
-        path: [
-            {
-                path: 'hz',
-                remark: '私有'
-            }
-        ]
-    },
-    {
-        zh: '金山云',
-        name: 'ksyun',
-        user: '2615749',
-        passwdfile: '~/.ssh/docker-ksyun',
-        reg: 'hub-cn-shanghai-2.kce.ksyun.com',
-        path: [
-            {
-                path: 'hzsq',
-                remark: '私有'
-            },
-            {
-                path: 'publ',
-                remark: '公有'
-            }
-        ]
-    }
-]
+// const typeList = [
+//     {
+//         zh: '阿里云',
+//         name: 'aliyun',
+//         user: 'a@h2',
+//         passwdfile: '~/.ssh/docker-alibabacloud',
+//         reg: 'registry.cn-hangzhou.aliyuncs.com',
+//         path: [
+//             {
+//                 path: 'h2',
+//                 remark: '私有'
+//             },
+//             {
+//                 path: 'h7',
+//                 remark: '私有'
+//             },
+//             {
+//                 path: 'h9',
+//                 remark: '私有'
+//             }
+//         ]
+//     },
+//     {
+//         zh: '腾讯云',
+//         name: 'qcloud',
+//         user: '100004236481',
+//         passwdfile: '~/.ssh/docker-tencentcloud',
+//         reg: 'ccr.ccs.tencentyun.com',
+//         path: [
+//             {
+//                 path: 'hzsq',
+//                 remark: '私有'
+//             }
+//         ]
+//     },
+//     {
+//         zh: 'Docker hub',
+//         name: 'dockerhub',
+//         user: 'hzsq',
+//         passwdfile: '~/.ssh/dockerioToken',
+//         reg: '',
+//         path: [
+//             {
+//                 path: 'hzsq',
+//                 remark: '公有'
+//             }
+//         ]
+//     },
+//     {
+//         zh: '华为云',
+//         name: 'huawei',
+//         user: 'cn-north-4@XRV15LDEFWCFLDH7GDXY',
+//         passwdfile: '~/.ssh/docker-huaweicloud',
+//         reg: 'swr.cn-north-4.myhuaweicloud.com',
+//         path: [
+//             {
+//                 path: 'hz',
+//                 remark: '私有'
+//             }
+//         ]
+//     },
+//     {
+//         zh: '百度云',
+//         name: 'baidu',
+//         user: '18999d5116464463a9cbc2c7f8f3cf01',
+//         passwdfile: '~/.ssh/docker-baiducloud',
+//         reg: 'registry.baidubce.com',
+//         path: [
+//             {
+//                 path: 'hz',
+//                 remark: '私有'
+//             }
+//         ]
+//     },
+//     {
+//         zh: '金山云',
+//         name: 'ksyun',
+//         user: '2615749',
+//         passwdfile: '~/.ssh/docker-ksyun',
+//         reg: 'hub-cn-shanghai-2.kce.ksyun.com',
+//         path: [
+//             {
+//                 path: 'hzsq',
+//                 remark: '私有'
+//             },
+//             {
+//                 path: 'publ',
+//                 remark: '公有'
+//             }
+//         ]
+//     }
+// ]
 
 interface IProxy {
     origin: string
@@ -156,6 +156,16 @@ const proxyList: IProxy[] = [
         list: [
             {
                 target: "codeload.p0t.top",
+                host: 'cloudflare',
+            }
+        ]
+    },
+    {
+        origin: "github.com/VSCodium/vscodium/releases/download/",
+        host: "github",
+        list: [
+            {
+                target: "mirrors.tuna.tsinghua.edu.cn/github-release/VSCodium/vscodium/",
                 host: 'cloudflare',
             }
         ]
@@ -246,7 +256,7 @@ const softwareList = [
             },
             {
                 name: 'codium',
-                link: "https://mirrors.tuna.tsinghua.edu.cn/github-release/VSCodium/vscodium/LatestRelease/#:~:text=VSCodium%2Dlinux%2Dx64",
+                link: "https://mirrors.tuna.tsinghua.edu.cn/github-release/VSCodium/vscodium/LatestRelease/",
                 icon: 'codium.svg',
             },
             {
@@ -308,7 +318,7 @@ const Page = () => {
     }
 
 
-    const [url, inputUrl] = useState('https://github.com/VSCodium/vscodium/releases/download/1.74.2.23007/VSCodium-linux-x64-1.74.2.23007.tar.gz')
+    const [url, inputUrl] = useState('https://github.com/VSCodium/vscodium/releases/download/1.75.0.23033/VSCodium-linux-x64-1.75.0.23033.tar.gz')
 
     return (
         <div className="common-box ">
