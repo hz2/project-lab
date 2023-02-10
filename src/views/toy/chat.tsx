5b509675204a1bcb03c129447c68b1e75d266483
hz <ht@live.se>
2023-02-10 14:40:56 +0800

    add openai chat

---
+++ src/views/toy/chat.tsx
@@ -0,0 +1,110 @@
+import { Button, Input, List, Spin } from "antd"
+import { ChangeEvent, useEffect, useRef, useState } from "react"
+import { query } from "./chatapi"
+import VirtualList, { ListRef } from 'rc-virtual-list';
+import "./chat.less"
+import { debounce } from "lodash";
+
+const { TextArea } = Input
+
+interface ChatItem {
+    type: 'me' | 'ai',
+    text: string,
+    t: number
+
+}
+
+const Chat = () => {
+
+    const [chatList, setChatList] = useState<ChatItem[]>([])
+    const [inputVal, setInputVal] = useState('')
+
+    const [loading, setLoading] = useState(false);
+
+
+    const sendTextInner = () => {
+        if (!inputVal) return
+        const list = [...chatList]
+        const newList = list.concat({
+            type: 'me',
+            text: inputVal,
+            t: Date.now()
+        })
+        setChatList(newList)
+        getResp(inputVal, newList)
+    }
+    const sendText = debounce(sendTextInner, 300)
+
+    const keydownHandler = (e: KeyboardEvent | React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (e.code === "Enter" && e.ctrlKey) sendText()
+    }
+    useEffect(() => {
+        document.addEventListener('keydown', keydownHandler);
+        return () => {
+            document.removeEventListener('keydown', keydownHandler);
+        }
+        // eslint-disable-next-line 
+    }, []);
+
+    const hasResp = () => {
+        setInputVal('')
+        setLoading(false)
+    }
+    const getResp = async (str: string, oldList: ChatItem[]) => {
+
+        setLoading(true)
+        try {
+            const response = await query({ prompt: str })
+            const newList = oldList.concat({
+                type: 'ai',
+                text: response && response.trim(),
+                t: Date.now()
+            })
+            setChatList(newList)
+            hasResp()
+            const index = newList.length - 1
+            listRef.current?.scrollTo({ index, align: "bottom" })
+        } catch (error) {
+            console.error(error);
+            hasResp()
+        }
+
+    }
+    const inputChange = ({
+        target: { value }
+    }: ChangeEvent<HTMLTextAreaElement>) => {
+        setInputVal(value)
+    }
+
+    const listRef = useRef<ListRef>(null)
+    return <div className="chat-page">
+        <List className="renderList">
+            <VirtualList
+                data={chatList}
+                height={window.innerHeight - 200}
+                itemHeight={47}
+                itemKey="t" ref={listRef}
+            >
+                {
+                    (x, i) => (<List.Item
+                        key={x.t + i}
+                        className={"item  " + (x.type === 'me' ? 'me' : 'ai')}
+                    >
+                        <div className="avatar">{x.type}</div>
+                        <div className="text">{x.text}</div>
+                    </List.Item>)
+                }
+            </VirtualList>
+        </List>
+        <Spin spinning={loading}>
+            <div className="bottom-block flex nowrap top">
+                <TextArea placeholder="请输入" rows={2} value={inputVal} onChange={inputChange} onKeyDown={keydownHandler} />
+                <Button type="primary" className="ml15" onClick={sendText}> Ctrl + Enter 发送</Button>
+            </div>
+        </Spin>
+    </div>
+
+}
+
+
+export default Chat
---
+++ src/views/toy/chatapi.ts
@@ -0,0 +1,87 @@
+// import { Configuration, OpenAIApi } from "openai";
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY || openai_api_key,
+// });
+// const openai = new OpenAIApi(configuration);
+
+// console.log("configuration", configuration);
+
+// export default async function (prompt: any) {
+//   //   if (!configuration.apiKey) {
+//   //     res.status(500).json({
+//   //       error: {
+//   //         message:
+//   //           "OpenAI API key not configured, please follow instructions in README.md",
+//   //       },
+//   //     });
+//   //     return;
+//   //   }
+
+//   //   const animal = req.body.animal || "";
+//   //   if (animal.trim().length === 0) {
+//   //     res.status(400).json({
+//   //       error: {
+//   //         message: "Please enter a valid animal",
+//   //       },
+//   //     });
+//   //     return;
+//   //   }
+
+//   try {
+//     const completion = await openai.createCompletion({
+//       model: "text-davinci-003",
+//       prompt: prompt ||
+//         'I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with "Unknown".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:',
+//       temperature: 0,
+//       max_tokens: 100,
+//       top_p: 1,
+//       frequency_penalty: 0.0,
+//       presence_penalty: 0.0,
+//       stop: ["\n"],
+//     });
+
+//     return completion.data.choices[0].text;
+
+//     // res.status(200).json({ result: completion.data.choices[0].text });
+//   } catch (error: any) {
+//     // Consider adjusting the error handling logic for your use case
+//     if (error.response) {
+//       console.error(error.response.status, error.response.data);
+//       //   res.status(error.response.status).json(error.response.data);
+//     } else {
+//       console.error(`Error with OpenAI API request: ${error.message}`);
+//       //   res.status(500).json({
+//       //     error: {
+//       //       message: "An error occurred during your request.",
+//       //     },
+//       //   });
+//     }
+//   }
+// }
+const DEFAULT_PARAMS = {
+  "model": "text-davinci-003",
+  "temperature": 0.7,
+  "max_tokens": 256,
+  "top_p": 1,
+  "frequency_penalty": 0,
+  "presence_penalty": 0,
+};
+
+export async function query(params = {}) {
+  const params_ = { ...DEFAULT_PARAMS, ...params };
+  const requestOptions = {
+    method: "POST",
+    mode: "cors" as RequestMode,
+    body: JSON.stringify(params_),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const response = await fetch(
+    "https://home.respok.com/chatai",
+    // "https://respok.com/ipinfo_io/default",
+    requestOptions,
+  );
+  const data = await response.json();
+  return data.choices[0].text;
+}
